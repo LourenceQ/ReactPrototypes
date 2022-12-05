e29f713a17fe777841fea4b408115fb3dc3dc81f
LawrenceQ <lawrenceqf@gmail.com>
2022-12-05 15:28:41 -0300

    Implementando binding

--- ceep/src/Components/FormularioCadastro.component.jsx
+++ ceep/src/Components/FormularioCadastro.component.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
 export class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaDeTitulor(evento) {
+    this.titulo = evento.target.value;
+    console.log(this.titulo);
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Título" />
+        <input
+          type="text"
+          placeholder="Título"
+          onChange={this.handleMudancaDeTitulor.bind(this)}
+        />
 
         <textarea rows={15} placeholder="Escreva aqui..." />
 
