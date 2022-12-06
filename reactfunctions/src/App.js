9daa7a0f0d3a06b796106312f9f393bc12875bf2
LawrenceQ <lawrenceqf@gmail.com>
2022-12-06 20:34:16 -0300

    React Functions First Commit

--- reactfunctions/src/App.js
+++ reactfunctions/src/App.js
@@ -1,16 +1,25 @@
-import React, { Component, Fragment } from "react";
-import "./App.css";
-import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>Formulário de cadastro.</h1>
-        <FormularioCadastro />        
-      </Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
