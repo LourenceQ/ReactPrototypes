826ec9201d14635f55dcbe93a8083a7035323281
LawrenceQ <lawrenceqf@gmail.com>
2022-12-07 20:51:50 -0300

    Implementando ofmrulário

--- reactfunctions/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ reactfunctions/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 
 function FormularioCadastro() {
+  let nome = "";
   return (
-    <form>
-      <label>Nome</label>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome);
+      }}
+    >
+      <label id="nome" label="Nome">
+        Nome
+      </label>
+      <input
+        onChange={(event) => {
+          nome = event.target.value;
+          if(nome.length>3) {
+            nome = nome.substrstring(0,3);
+          }
+        }}
+        type="text"
+      ></input>
+
+      <label id="sobrenome" label="Sobrenome">
+        Sobrenome
+      </label>
       <input type="text"></input>
 
-      <label>Sobrenome</label>
-      <input type="text"></input>
-
-      <label>CPF</label>
+      <label id="cpf" label="CPF">
+        CPF
+      </label>
       <input type="text"></input>
 
       <label>Promoçoes</label>
