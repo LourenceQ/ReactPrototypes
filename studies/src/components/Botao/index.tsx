51a81b94be8dc997388c71bceadb260806176bb5
LawrenceQ <lawrenceqf@gmail.com>
2022-12-17 09:14:04 -0300

    modularizando estilos

--- studies/src/components/Botao/index.tsx
+++ studies/src/components/Botao/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import "./style.scss";
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component {
   render() {
-    return <button className="botao">Enviar</button>;
+    return (
+    <button className={style.Botao}>
+      Enviar
+      </button>
+    )
   }
 }
 
--- studies/src/pages/App.tsx
+++ studies/src/pages/App.tsx
@@ -4,10 +4,11 @@ import "../App.css";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
+import style from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.AppStyle}>
       <Formulario />
       <Lista />
     </div>
