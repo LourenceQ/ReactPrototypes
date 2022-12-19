4d2717e1c794d25797460ba38184ab3ad70c8109
Lourence Queiroz Fofano <lourence.fofano@energisa.com.br>
2022-12-19 19:27:28 -0300

    .

--- studies/src/components/Botao/index.tsx
+++ studies/src/components/Botao/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component {
+class Botao extends React.Component<IProps> {
   render() {
-    return (
-    <button className={style.Botao}>
-      Enviar
-      </button>
-    )
+    return <button className={style.Botao}>{this.props.children}</button>;
   }
 }
 
 export default Botao;
+
+interface IProps {
+  children: React.ReactNode;
+}
