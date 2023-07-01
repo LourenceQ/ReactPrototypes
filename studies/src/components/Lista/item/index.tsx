7f1e649717341795de1781da6e8266ea6af51eab
LourenceQ <lawrenceqf@gmail.com>
2023-06-30 22:33:05 -0300

    Implementando funcionalidade para selecionar tarefa

--- studies/src/components/Lista/item/index.tsx
+++ studies/src/components/Lista/item/index.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import style from "../Lista.module.scss"
 import { ITarefa } from "../../../types/tarefa";
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item(
+  { 
+    tarefa
+    , tempo
+    , selecionado
+    , completado
+    , id
+    , selecionaTarefa
+  }: Props) {
   return (
-    <li className={style.item}>
+    <li className={style.item} onClick={() => selecionaTarefa({
+      tarefa, tempo, selecionado, completado, id
+    })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
