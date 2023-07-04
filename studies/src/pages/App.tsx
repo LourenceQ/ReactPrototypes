1f61001d8e3f32c96c87d4ad93bd354a0a73639d
LourenceQ <lawrenceqf@gmail.com>
2023-07-04 18:26:28 -0300

    teste

--- studies/src/pages/App.tsx
+++ studies/src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../App.css";
 import Botao from "../components/Botao";
-import { Cronometro } from "../components/Cronometro/date";
+import { Cronometro } from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
 import { ITarefa } from "../types/tarefa";
