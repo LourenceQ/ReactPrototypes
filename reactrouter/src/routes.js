c8f68ea5de25787caeff6fd5071028b9d5a2d028
LourenceQ <lawrenceqf@gmail.com>
2023-07-13 22:03:14 -0300

    Reaproveitando header e refatorando responsabilidade de estilos de header

--- reactrouter/src/routes.js
+++ reactrouter/src/routes.js
@@ -1,17 +1,23 @@
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
-import Inicio from 'pages/Inicio';
+import PaginaPadrao from './components/PaginaPadrao';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inicio from 'pages/Inicio';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio/>} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
+    <main>
+      
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio/>} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
 
-    </Router>
+      </Router>
+    </main>
   );
 }
